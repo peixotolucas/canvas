8ab3ce81c1dceb285e150748c8dd26474203cf54
Lucas <peixotolucas516@gmail.com>
2023-01-15 18:51:25 -0300

    health, timer implemented, enemy health bar decreases now

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
     }
     this.color = color
     this.isAttacking
+    this.health = 100
   }
 
   draw() {
@@ -153,7 +154,8 @@ function animate() {
     player.isAttacking
     ) {
     player.isAttacking = false
-    console.log('POW')
+    enemy.health -= 20
+    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
   }
 
   if (
